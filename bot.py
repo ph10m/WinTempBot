1b9af4f7f25a2a935f0fafb2decbff3079381826
Tollef Jørgensen <tollefj@gmail.com>
2017-06-12 18:09:34 +0200

    temperature logic

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 from twitter import *
 import wmi
+from time import time
 
 c_key = 'PFuRYoFCdRvVqG3UMHfnfEXvZ'
 c_secret = 'Hiv0VJCXuPbRNvhe7LhChIQJPJroBj5m4mPTwdp7cNaAwdYPBw'
@@ -14,10 +15,23 @@ def post(x):
 #post("gpu temps here goes here plz test 1xD23")
 
 w = wmi.WMI(namespace="root\OpenHardwareMonitor")
-temperature_infos = w.Sensor()
-for sensor in temperature_infos:
-    if sensor.SensorType==u'Temperature':
-        if 'package' in sensor.Name.lower():
-            print 'CPU temp is',sensor.Value
-        elif 'gpu' in sensor.Name.lower():
-            print 'GPU temp is',round(sensor.Value,2)
+cpu_temp, gpu_temp, mobo_temp = -1,-1,-1
+while True:
+    temps = w.Sensor()
+    for sensor in temps:
+        if sensor.SensorType==u'Temperature':
+            if 'package' in sensor.Name.lower():
+                cpu_temp = int(sensor.Value)
+            elif 'gpu' in sensor.Name.lower():
+                gpu_temp = int(sensor.Value)
+            elif 'motherboard' in sensor.Name.lower():
+                mobo_temp = int(sensor.Value)
+
+    temp_string = 'CPU: '+str(cpu_temp)+'\nGPU: '+str(gpu_temp) +'\nMB: '+str(mobo_temp)
+    if cpu_temp>20 and gpu_temp>20 and mobo_temp>20:
+        # everything is ok
+        post(temp_string)
+    if cpu_temp>50 or gpu_temp>60 or mobo_temp>45:
+        # critical
+        post('High temperatures detected, check the computer!')
+    sleep(10)
