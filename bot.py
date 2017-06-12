49da3a2eba0a2ab0b34ec0f8b05793e34f7b6755
Tollef Jørgensen <tollefj@gmail.com>
2017-06-12 17:29:12 +0200

    initial setup

---
+++ bot.py
@@ -0,0 +1,14 @@
+import os
+from twitter import *
+
+
+c_key = 'PFuRYoFCdRvVqG3UMHfnfEXvZ'
+c_secret = 'Hiv0VJCXuPbRNvhe7LhChIQJPJroBj5m4mPTwdp7cNaAwdYPBw'
+a_token = '4915565709-7sm13JTJadJziX6SmnNPwidk6g7apuuG5dgPSnC'
+a_secret = '8j4CZfkRXRDJ5ZnkRAWGFCawp8JbgxNolWLvK2qZQ0A2H'
+
+t = Twitter(auth=OAuth(a_token,a_secret,c_key,c_secret))
+def post(x):
+    t.statuses.update(status=x)
+    print 'Updated status: '+x
+post("gpu temps here goes here plz test 1xD23")
