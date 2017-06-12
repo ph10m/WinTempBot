4d9fbeadada6dda4d30d56ef86b03734ac87c3b7
Tollef Jørgensen <tollefj@gmail.com>
2017-06-12 18:02:04 +0200

    fetch cpu and gpu temps

--- bot.py
+++ bot.py
@@ -17,5 +17,7 @@ w = wmi.WMI(namespace="root\OpenHardwareMonitor")
 temperature_infos = w.Sensor()
 for sensor in temperature_infos:
     if sensor.SensorType==u'Temperature':
-        print(sensor.Name)
-        print(sensor.Value)
+        if 'package' in sensor.Name.lower():
+            print 'CPU temp is'+sensor.Value
+        elif 'gpu' in sensor.Name.lower():
+            print 'GPU temp is'+round(sensor.Value,2)
