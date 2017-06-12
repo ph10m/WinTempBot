db97e0498163f29cd0def79b1fcb66c62416b643
Tollef Jørgensen <tollefj@gmail.com>
2017-06-12 17:59:56 +0200

    added sensors

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 from twitter import *
-
+import wmi
 
 c_key = 'PFuRYoFCdRvVqG3UMHfnfEXvZ'
 c_secret = 'Hiv0VJCXuPbRNvhe7LhChIQJPJroBj5m4mPTwdp7cNaAwdYPBw'
@@ -11,4 +11,11 @@ t = Twitter(auth=OAuth(a_token,a_secret,c_key,c_secret))
 def post(x):
     t.statuses.update(status=x)
     print 'Updated status: '+x
-post("gpu temps here goes here plz test 1xD23")
+#post("gpu temps here goes here plz test 1xD23")
+
+w = wmi.WMI(namespace="root\OpenHardwareMonitor")
+temperature_infos = w.Sensor()
+for sensor in temperature_infos:
+    if sensor.SensorType==u'Temperature':
+        print(sensor.Name)
+        print(sensor.Value)
