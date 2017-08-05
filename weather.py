254aed5f3b29831b3e04e4e3960b1e202c886da0
Tollef Jørgensen <tollefj@gmail.com>
2017-08-06 01:46:09 +0200

    weather constants

---
+++ weather.py
@@ -0,0 +1,2 @@
+TRONDHEIM = 'http://openweathermap.org/data/2.5/weather?id=3133880&appid=b1b15e88fa797225412429c1c50c122a1'
+HARSTAD = 'http://openweathermap.org/data/2.5/weather?id=6453319&appid=b1b15e88fa797225412429c1c50c122a1'
